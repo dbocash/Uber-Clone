61caa88a4d42b99a90a7da72a9d7f3a72495b381
Daniel Bocash <daniel.bocash@gmail.com>
2024-11-12 18:16:25 -0500

    Commit before needing to build a project

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,17 +1,17 @@
 import { Link, Stack } from "expo-router";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
 export default function NotFoundScreen() {
   return (
-    <>
+    <View>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text type="title">This screen doesn't exist.</Text>
+        <Text>This screen doesn't exist.</Text>
         <Link href="/" style={styles.link}>
-          <Text type="link">Go to home screen!</Text>
+          <Text>Go to home screen!</Text>
         </Link>
       </View>
-    </>
+    </View>
   );
 }
 
